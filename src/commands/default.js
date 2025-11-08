8dd32d2a2373e496ed23694ca3cb2c66160069b1
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2025-11-08 12:08:28 +0100

    Refactor command execution and improve subcommand detection
    - Replace executeCommand with execSyncCommand for better error handling
    - Add fallback help flag attempts (-h, help)
    - Enhance getSubCommands with secondary regex pattern for broader
      matching
    - Rename getCommands to getSubCommands for clarity

--- src/commands/default.js
+++ src/commands/default.js
@@ -4,35 +4,59 @@ const { execSync } = require("node:child_process");
 const { joinArgs, filterArgs, getLastCommand } = require("../utils");
 
 function getDefaultCommands(args) {
-  try {
-    return executeCommand(args);
-  } catch {
-    return executeCommand(args.slice(0, args.length - 1));
-  }
+  return getSubCommandsAndOptions(args);
 }
 
-function executeCommand(args) {
+function execSyncCommand(command) {
+  const exec = (cmd) =>
+    execSync(cmd, {
+      encoding: "utf8",
+      stdio: "pipe",
+    });
+
+  try {
+    if (exec(command + " --help 2>/dev/null")) {
+      return exec(command + " --help");
+    }
+
+    if (exec(command + " -h 2>/dev/null")) {
+      return exec(command + " -h");
+    }
+
+    if (exec(command + " help 2>/dev/null")) {
+      return exec(command + " help");
+    }
+  } catch (error) {
+    return (
+      error.output?.toString() ||
+      error.stdout?.toString() ||
+      error.stderr?.toString() ||
+      []
+    );
+  }
+
+  return [];
+}
+
+function getSubCommandsAndOptions(args) {
   if (args.length === 0) {
     return [];
   }
 
   const filteredArgs = joinArgs(getLastCommand(filterArgs(args)));
 
-  const output = execSync(filteredArgs + " --help", {
-    encoding: "utf8",
-    stdio: "pipe",
-  });
+  const output = execSyncCommand(filteredArgs);
 
   const options = getOptions(output);
-  const commands = getCommands(output);
+  const commands = getSubCommands(output);
 
   const result = [...commands, ...options];
 
   if (result.length === 0) {
     const lastCommand = args.pop();
 
-    return executeCommand(args.slice(0, args.length - 1)).filter((command) =>
-      command.includes(lastCommand),
+    return getSubCommandsAndOptions(args.slice(0, args.length - 1)).filter(
+      (command) => command.includes(lastCommand),
     );
   }
 
@@ -45,10 +69,16 @@ function getOptions(output) {
   );
 }
 
-function getCommands(output) {
-  return Array.from(output.matchAll(/^\s{2,}([a-z0-9-]+)\s{2,}/gm)).map(
-    (command) => command[1],
-  );
+function getSubCommands(output) {
+  const firtTryToGetCommands = Array.from(
+    output.matchAll(/^\s{2,}([a-z0-9-]+)\s{2,}/gm),
+  ).map((command) => command[1]);
+
+  const secondTryToGetCommands = [
+    ...output.matchAll(/\b\w[\w-]+(?=\s{2,}|,|\s*$)/g),
+  ].map((command) => command[0]);
+
+  return [...new Set([...firtTryToGetCommands, ...secondTryToGetCommands])];
 }
 
 module.exports = {

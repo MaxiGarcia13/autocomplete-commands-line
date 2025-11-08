ea50e85accd5badb78d08b58f6692913d0683892
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2025-11-08 12:09:10 +0100

    Filter command line args and add DASH constant

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 // Grab the words typed so far (Zsh passes them as args)
 const { getDefaultCommands, readPackageJson } = require("./commands");
 const { joinArgs } = require("./utils");
-const { NEWLINE } = require("./utils/const");
+const { NEWLINE, DASH } = require("./utils/const");
 
-const args = process.argv.slice(2);
+const args = process.argv
+  .slice(2)
+  .map((arg) => arg.trim())
+  .filter((arg) => arg.length > 0 && !arg.includes(DASH));
 
 let suggestions = [];
 
